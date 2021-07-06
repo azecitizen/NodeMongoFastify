6daa2275b5ee9df32463398f6cac695385722049
unknown <ribeyronhugo@gmail.com>
2021-07-06 15:57:41 +0200

    Ajout de .gitignore

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
+// cnx bdd
+fastify.register(require('fastify-mongodb'), {
+  // force to close the mongodb connection when app stopped
+  // the default value is false
+  forceClose: true,
+  
+  url: 'mongodb://localhost:27017/superheroes'
+
+})
+
 // Declare a route
 fastify.get('/', async (request, reply) => {
     // ici on retourne un obj js qui va etre converti en json (JS object notation)
@@ -11,14 +21,27 @@ fastify.get('/me', function() {
     return { 
         prenom: 'Hugo',
         nom: 'R',
-        taf: 'glandeur pro'
+        taf: 'pro'
     }
 })
 
 const avengers = ['Batman', 'Wolverine', 'Flash']
 
-fastify.get('/avengers', () => {
+// get, obtient heroes
+fastify.get('/heroes', () => {
     return avengers
+})
+
+// post ajout un hero
+fastify.post('/heroes', (request,reply) => {
+  const col = fastify.mongo.db.collection("heroes")
+  col.insertOne(request.body)
+  // col.insertOne({
+  //   name: request.body.name,
+  //   powerstats: request.body.powerstats
+  // })
+  return null
+  // <> a 42 reply.send(null)
 })
 
 // Run the server!
